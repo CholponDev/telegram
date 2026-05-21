550b1278483ff1143a7fa6d1020b0dbeeda0d08f
Cholpon <januzakova.cholpon@gmail.com>
2026-05-21 10:34:22 +0600

    add project files

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC9KduorN2KPVhTRNBcKuddJe52dFd7SDU",
+  authDomain: "telegram-818ec.firebaseapp.com",
+  projectId: "telegram-818ec",
+  storageBucket: "telegram-818ec.firebasestorage.app",
+  messagingSenderId: "403661572454",
+  appId: "1:403661572454:web:df2967cb39653f0a8020a7",
+  measurementId: "G-MCPD40X4BK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from '../styles/Home.module.css'
+
+function Home() {
+  return (
+    <div className={style.home}>
+        <div className={style.container}>
+            <img src="" alt="" />
+            <div className={style.block}>
+                <h1>home</h1>
+                <p>about</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
